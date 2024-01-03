799d85a7bb1380fa46530a961db3aa7b480ac6a8
kate <kas493@cornell.edu>
2024-01-03 11:58:58 -0500

    generalize blendphase to accept an arbitrary number of child phase functions

--- src/phase/tests/test_blendphase.py
+++ src/phase/tests/test_blendphase.py
@@ -6,9 +6,9 @@ def test01_create(variant_scalar_rgb):
     phase = mi.load_dict(
         {
             "type": "blendphase",
-            "phase1": {"type": "isotropic"},
-            "phase2": {"type": "isotropic"},
-            "weight": 0.2,
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "isotropic"},
+            "weight_0": 0.8,
         }
     )
     assert phase is not None
@@ -17,9 +17,9 @@ def test01_create(variant_scalar_rgb):
     phase = mi.load_dict(
         {
             "type": "blendphase",
-            "phase1": {"type": "isotropic"},
-            "phase2": {"type": "hg"},
-            "weight": 0.2,
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg"},
+            "weight_0": 0.8,
         }
     )
     assert phase is not None
@@ -31,16 +31,16 @@ def test01_create(variant_scalar_rgb):
     )
 
 
-def test02_eval_all(variant_scalar_rgb):
-    weight = 0.2
+def test02_eval_2(variant_scalar_rgb):
+    weight_0 = 0.8
     g = 0.2
 
     phase = mi.load_dict(
         {
             "type": "blendphase",
-            "phase1": {"type": "isotropic"},
-            "phase2": {"type": "hg", "g": g},
-            "weight": weight,
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg", "g": g},
+            "weight_0": weight_0,
         }
     )
 
@@ -54,23 +54,22 @@ def test02_eval_all(variant_scalar_rgb):
     ctx = mi.PhaseFunctionContext(None)
 
     # Evaluate the blend of both components
-    expected = (1.0 - weight) * dr.inv_four_pi + weight * dr.inv_four_pi * (1.0 - g) / (
-        1.0 + g
-    ) ** 2
+    expected = weight_0 * dr.inv_four_pi + (1 - weight_0) * dr.inv_four_pi * \
+        (1.0 - g) / (1.0 + g) ** 2
     value = phase.eval_pdf(ctx, mei, wo)[0]
     assert dr.allclose(value, expected)
 
 
-def test03_sample_all(variants_all_rgb):
-    weight = 0.2
+def test03_sample_2(variants_all_rgb):
+    weight_0 = 0.8
     g = 0.2
 
     phase = mi.load_dict(
         {
             "type": "blendphase",
-            "phase1": {"type": "isotropic"},
-            "phase2": {"type": "hg", "g": g},
-            "weight": weight,
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg", "g": g},
+            "weight_0": weight_0,
         }
     )
 
@@ -85,27 +84,32 @@ def test03_sample_all(variants_all_rgb):
     # Sample using two different values of 'sample1' and make sure correct
     # components are chosen.
 
-    # -- Sample above weight: first component (isotropic) is selected
+    # -- Sample below weight: first component (isotropic) is selected
     expected_a = dr.inv_four_pi
     wo_a, w_a, pdf_a = phase.sample(ctx, mei, 0.3, [0.5, 0.5])
     assert dr.allclose(pdf_a, expected_a)
 
-    # -- Sample below weight: second component (HG) is selected
+    # -- Sample above weight: second component (HG) is selected
     expected_b = dr.inv_four_pi * (1 - g) / (1 + g) ** 2
-    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.1, [0, 0])
+    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.9, [0, 0])
     assert dr.allclose(pdf_b, expected_b)
 
 
-def test04_eval_components(variant_scalar_rgb):
-    weight = 0.2
-    g = 0.2
+def test04_eval_3(variant_scalar_rgb):
+    weight_0 = 0.3
+    weight_1 = 0.5
+    weight_2 = 1 - (weight_0 + weight_1)
+    g_1 = 0.2
+    g_2 = 0.9
 
     phase = mi.load_dict(
         {
             "type": "blendphase",
-            "phase1": {"type": "isotropic"},
-            "phase2": {"type": "hg", "g": g},
-            "weight": weight,
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg", "g": g_1},
+            "phase_2": {"type": "hg", "g": g_2},
+            "weight_0": weight_0,
+            "weight_1": weight_1
         }
     )
 
@@ -118,31 +122,28 @@ def test04_eval_components(variant_scalar_rgb):
     wo = [0, 0, 1]
     ctx = mi.PhaseFunctionContext(None)
 
-    # Evaluate the two components separately
-
-    ctx.component = 0
-    value0, pdf0 = phase.eval_pdf(ctx, mei, wo)
-    expected0 = (1 - weight) * dr.inv_four_pi
-    assert dr.allclose(value0, expected0)
-    assert dr.allclose(value0, pdf0)
-
-    ctx.component = 1
-    value1, pdf1 = phase.eval_pdf(ctx, mei, wo)
-    expected1 = weight * dr.inv_four_pi * (1.0 - g) / (1.0 + g) ** 2
-    assert dr.allclose(value1, expected1)
-    assert dr.allclose(value1, pdf1)
+    # Evaluate the blend of all components
+    expected = weight_0 * dr.inv_four_pi + \
+               weight_1 * dr.inv_four_pi * (1.0 - g_1) / (1.0 + g_1) ** 2 + \
+               weight_2 * dr.inv_four_pi * (1.0 - g_2) / (1.0 + g_2) ** 2
+    value = phase.eval_pdf(ctx, mei, wo)[0]
+    assert dr.allclose(value, expected)
 
 
-def test05_sample_components(variant_scalar_rgb):
-    weight = 0.2
-    g = 0.2
+def test05_sample_3(variants_all_rgb):
+    weight_0 = 0.3
+    weight_1 = 0.5
+    g_1 = 0.2
+    g_2 = 0.9
 
     phase = mi.load_dict(
         {
             "type": "blendphase",
-            "phase1": {"type": "isotropic"},
-            "phase2": {"type": "hg", "g": g},
-            "weight": weight,
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg", "g": g_1},
+            "phase_2": {"type": "hg", "g": g_2},
+            "weight_0": weight_0,
+            "weight_1": weight_1
         }
     )
 
@@ -154,27 +155,131 @@ def test05_sample_components(variant_scalar_rgb):
 
     ctx = mi.PhaseFunctionContext(None)
 
-    # Sample using two different values of 'sample1' and make sure correct
+    # Sample using different values of 'sample1' and make sure correct
+    # components are chosen.
+
+    # -- Sample below weight_0: first component (isotropic) is selected
+    expected_a = dr.inv_four_pi
+    wo_a, w_a, pdf_a = phase.sample(ctx, mei, 0.1, [0.5, 0.5])
+    assert dr.allclose(pdf_a, expected_a)
+
+    # -- Sample below (weight_0 + weight_1): second component (HG_1) is selected
+    expected_b = dr.inv_four_pi * (1 - g_1) / (1 + g_1) ** 2
+    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.5, [0, 0])
+    assert dr.allclose(pdf_b, expected_b)
+
+    # -- Sample above (weight_0 + weight_1): third component (HG_2) is selected
+    expected_c = dr.inv_four_pi * (1 - g_2) / (1 + g_2) ** 2
+    wo_c, w_c, pdf_c = phase.sample(ctx, mei, 0.9, [0, 0])
+    assert dr.allclose(pdf_c, expected_c)
+
+
+def test06_eval_components(variant_scalar_rgb):
+    weight_0 = 0.3
+    weight_1 = 0.5
+    weight_2 = 1 - (weight_0 + weight_1)
+    g_1 = 0.2
+    g_2 = 0.9
+
+    phase = mi.load_dict(
+        {
+            "type": "blendphase",
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg", "g": g_1},
+            "phase_2": {"type": "hg", "g": g_2},
+            "weight_0": weight_0,
+            "weight_1": weight_1
+        }
+    )
+
+    mei = mi.MediumInteraction3f()
+    mei.t = 0.1
+    mei.p = [0, 0, 0]
+    mei.sh_frame = mi.Frame3f([0, 0, 1])
+    mei.wi = [0, 0, 1]
+
+    wo = [0, 0, 1]
+    ctx = mi.PhaseFunctionContext(None)
+
+    # Evaluate the components separately
+
+    ctx.component = 0
+    value0, pdf0 = phase.eval_pdf(ctx, mei, wo)
+    expected0 = weight_0 * dr.inv_four_pi
+    assert dr.allclose(value0, expected0)
+    assert dr.allclose(value0, pdf0)
+
+    ctx.component = 1
+    value1, pdf1 = phase.eval_pdf(ctx, mei, wo)
+    expected1 = weight_1 * dr.inv_four_pi * (1.0 - g_1) / (1.0 + g_1) ** 2
+    assert dr.allclose(value1, expected1)
+    assert dr.allclose(value1, pdf1)
+
+    ctx.component = 2
+    value2, pdf2 = phase.eval_pdf(ctx, mei, wo)
+    expected2 = weight_2 * dr.inv_four_pi * (1.0 - g_2) / (1.0 + g_2) ** 2
+    assert dr.allclose(value2, expected2)
+    assert dr.allclose(value2, pdf2)
+
+
+def test07_sample_components(variant_scalar_rgb):
+    weight_0 = 0.3
+    weight_1 = 0.5
+    weight_2 = 1 - (weight_0 + weight_1)
+    g_1 = 0.2
+    g_2 = 0.9
+
+    phase = mi.load_dict(
+        {
+            "type": "blendphase",
+            "phase_0": {"type": "isotropic"},
+            "phase_1": {"type": "hg", "g": g_1},
+            "phase_2": {"type": "hg", "g": g_2},
+            "weight_0": weight_0,
+            "weight_1": weight_1
+        }
+    )
+
+    mei = mi.MediumInteraction3f()
+    mei.t = 0.1
+    mei.p = [0, 0, 0]
+    mei.sh_frame = mi.Frame3f([0, 0, 1])
+    mei.wi = [0, 0, 1]
+
+    ctx = mi.PhaseFunctionContext(None)
+
+    # Sample using different values of 'sample1' and make sure correct
     # components are chosen.
 
     # -- Select component 0: first component is always sampled
     ctx.component = 0
 
-    expected_a = (1 - weight) *  dr.inv_four_pi
+    expected_a = weight_0 *  dr.inv_four_pi
     wo_a, w_a, pdf_a = phase.sample(ctx, mei, 0.3, [0.5, 0.5])
     assert dr.allclose(pdf_a, expected_a)
 
-    expected_b = (1 - weight) *  dr.inv_four_pi
-    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.1, [0.5, 0.5])
+    expected_b = weight_0 *  dr.inv_four_pi
+    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.9, [0.5, 0.5])
     assert dr.allclose(pdf_b, expected_b)
 
     # -- Select component 1: second component is always sampled
     ctx.component = 1
 
-    expected_a = weight *  dr.inv_four_pi * (1 - g) / (1 + g) ** 2
+    expected_a = weight_1 *  dr.inv_four_pi * (1 - g_1) / (1 + g_1) ** 2
     wo_a, w_a, pdf_a = phase.sample(ctx, mei, 0.3, [0.0, 0.0])
     assert dr.allclose(pdf_a, expected_a)
 
-    expected_b = weight * dr.inv_four_pi * (1 - g) / (1 + g) ** 2
-    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.1, [0.0, 0.0])
+    expected_b = weight_1 * dr.inv_four_pi * (1 - g_1) / (1 + g_1) ** 2
+    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.9, [0.0, 0.0])
+    assert dr.allclose(pdf_b, expected_b)
+
+    # -- Select component 2: third component is always sampled
+    ctx.component = 2
+
+    expected_a = weight_2 *  dr.inv_four_pi * (1 - g_2) / (1 + g_2) ** 2
+    wo_a, w_a, pdf_a = phase.sample(ctx, mei, 0.3, [0.0, 0.0])
+    assert dr.allclose(pdf_a, expected_a)
+
+    expected_b = weight_2 * dr.inv_four_pi * (1 - g_2) / (1 + g_2) ** 2
+    wo_b, w_b, pdf_b = phase.sample(ctx, mei, 0.9, [0.0, 0.0])
     assert dr.allclose(pdf_b, expected_b)
